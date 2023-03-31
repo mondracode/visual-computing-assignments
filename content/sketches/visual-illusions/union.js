4a815cefd99a375fecbd8c4e61390f7e828b9c12
Santiago Mondragón Gómez <santiago.mondragon@mercadolibre.com.co>
2023-03-30 22:30:20 -0500

    fix image baseURL

--- content/sketches/visual-illusions/union.js
+++ content/sketches/visual-illusions/union.js
@@ -9,7 +9,7 @@ const NUMBER_OF_BIRD_LAYERS = 3;
 
 function preload() {
   // Carga la imagen antes de que el sketch se ejecute
-  let hostURL = "https://mondracode.github.io/visual-computing-assignments/";
+  let hostURL = "https://mondracode.github.io/visual-computing-assignments";
 
   bgImg = loadImage(`${hostURL}/assets/cielo_background.jpg`);
   cloudImg = loadImage(`${hostURL}/assets/cloud.webp`);
@@ -19,21 +19,21 @@ function preload() {
 
 function setup() {
   createCanvas(500, 500);
-  
+
   // Setup the cloud layers and span the diferent clouds
   for (let layer = 1; layer <= NUMBER_OF_CLOUD_LAYERS; layer++) {
     for (let i = 0; i < 5; i++) {
       clouds.push(new Cloud(layer));
     }
   }
-  
+
   // Setup the birds layers and span the diferent birds
   for (let layer = 1; layer <= NUMBER_OF_BIRD_LAYERS; layer++) {
     for (let i = 0; i < 2; i++) {
       birds.push(new Birds(layer));
     }
   }
-  
+
   // Setup the airplane
   airplanes.push(new Airplane(1));
 }
@@ -41,35 +41,35 @@ function setup() {
 function draw() {
   // Use the background image on canva
   background(bgImg);
-  
+
   // print the different clouds
   for (const cloud of clouds) {
     cloud.draw();
   }
-  
+
   // print the different birds
-  for (const bird of birds){
+  for (const bird of birds) {
     bird.draw();
   }
-  
+
   // print the airplane
-  for (const airplane of airplanes){
+  for (const airplane of airplanes) {
     airplane.draw();
   }
 }
 
 class Birds {
-  constructor(layer){
+  constructor(layer) {
     this.layer = layer;
     this.x = random(-border, width + border);
     this.y = random(-border, height + border);
   }
-  
-  draw(){
-    let deltaX = 0;    
+
+  draw() {
+    let deltaX = 0;
     deltaX = this.layer;
     this.x -= deltaX;
-    
+
     // out of screen control
     if (this.x < -(this.layer * 30 * 1.5)) {
       this.x = width + border;
@@ -78,23 +78,23 @@ class Birds {
       this.x = -border;
       this.y = random(0, height);
     }
-    
+
     image(birdsGif, this.x, this.y, this.layer * 30 * 1.5, this.layer * 30 * 1);
   }
 }
 
 class Cloud {
-  constructor(layer){
+  constructor(layer) {
     this.layer = layer;
     this.x = random(-border, width + border);
     this.y = random(-border, height + border);
   }
-  
-  draw(){
-    let deltaX = 0;    
+
+  draw() {
+    let deltaX = 0;
     deltaX = this.layer;
     this.x += deltaX;
-    
+
     // out of screen control
     if (this.x < -border) {
       this.x = border;
@@ -103,7 +103,7 @@ class Cloud {
       this.x = -border;
       this.y = random(0, height);
     }
-    
+
     image(cloudImg, this.x, this.y, this.layer * 20 * 1.5, this.layer * 20 * 1);
   }
 }
@@ -114,7 +114,7 @@ class Airplane {
     this.x = - this.layer * 250 * 1.5;
     this.y = height - this.layer * 250 * 1;
   }
-  
+
   draw() {
     let deltaX = 0;
     let deltaY = 0;
@@ -122,7 +122,7 @@ class Airplane {
     deltaY = this.layer * 0.5;
     this.x += deltaX;
     this.y -= deltaY;
-    
-    image(airplaneImg, this.x, this.y, this.layer * 250 * 1.5, this.layer * 250 * 1); 
+
+    image(airplaneImg, this.x, this.y, this.layer * 250 * 1.5, this.layer * 250 * 1);
   }
 }
