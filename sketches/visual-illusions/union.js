497ed64405f1d0488adb216619ed40304da5630a
pmoros <pmoros@users.noreply.github.com>
2023-04-03 00:16:05 +0000

    deploy: c017a5dba06ad4f6f2d431dfb00102bdc4e6e798

--- sketches/visual-illusions/union.js
+++ sketches/visual-illusions/union.js
@@ -4,12 +4,17 @@ const clouds = [];
 const birds = [];
 const airplanes = [];
 const border = 50;
+const intervalLowerBound = 100;
+const intervalUpperBound = 260;
+const numberOfIntervals = 4;
 const NUMBER_OF_CLOUD_LAYERS = 2;
 const NUMBER_OF_BIRD_LAYERS = 3;
+var depthOfField = 0;
 
 function preload() {
   // Carga la imagen antes de que el sketch se ejecute
-  let hostURL = "https://mondracode.github.io/visual-computing-assignments";
+  // let hostURL = "https://mondracode.github.io/visual-computing-assignments";
+  let hostURL = "http://localhost:1313/visual-computing-assignments";
 
   bgImg = loadImage(`${hostURL}/assets/cielo_background.jpg`);
   cloudImg = loadImage(`${hostURL}/assets/cloud.webp`);
@@ -17,6 +22,14 @@ function preload() {
   airplaneImg = loadImage(`${hostURL}/assets/plane.png`);
   skyscrapers = loadImage(`${hostURL}/assets/skyscrapers.png`);
   manGif = loadImage(`${hostURL}/assets/fallingman.gif`);
+
+  // Load blurred images
+  blurredSkyscrapers = [];
+  for (let i = 0; i < numberOfIntervals; i++) {
+    blurredSkyscrapers.push(
+      loadImage(`${hostURL}/assets/skyscrapers_blur_00${i}.png`)
+    );
+  }
 }
 
 function setup() {
@@ -44,9 +57,27 @@ function setup() {
 
   // Setup man
   fallingMan = new Man(3);
+
+  // Setup the blurred plane
+  blurredPlaneFactory = new BlurredPlaneFactory(
+    intervalLowerBound,
+    intervalUpperBound,
+    numberOfIntervals
+  );
+  blurredPlaneIntervals = blurredPlaneFactory.generateIntervals();
 }
 
 function draw() {
+  // get the interval for the current depth
+  let interval = blurredPlaneFactory.getDiscreteIntervalForDepth(depthOfField);
+  console.log(interval);
+  // set the blurred plane
+  let blurredSkyscraper =
+    blurredPlaneFactory.getBlurredPlaneForQuantizedInterval(
+      interval,
+      "skyscrapers"
+    );
+
   // Use the background image on canva
   background(bgImg);
 
@@ -61,7 +92,7 @@ function draw() {
   }
 
   // draw linear perspective skyscrapers
-  skyscrapersLayer.draw();
+  skyscrapersLayer.draw(blurredSkyscraper);
 
   // draw falling man
   fallingMan.draw();
@@ -69,6 +100,47 @@ function draw() {
   // print the different birds
   for (const bird of birds) {
     bird.draw();
+  }
+
+  // draw blurred plane
+}
+
+class BlurredPlaneFactory {
+  constructor(rangeInit, rangeEnd, n) {
+    this.a = rangeInit;
+    this.b = rangeEnd;
+    this.n = n;
+  }
+
+  generateIntervals() {
+    const intervalDelta = (this.b - this.a) / this.n;
+    const intervals = Array.from(
+      { length: this.n },
+      (_, i) => this.a + (i + 1) * intervalDelta
+    );
+
+    this.intervals = intervals;
+
+    return this.intervals;
+  }
+
+  getDiscreteIntervalForDepth(depth) {
+    let interval = 0;
+    for (let i = 0; i < this.n; i++) {
+      if (depth > this.intervals[i]) {
+        interval = i;
+      }
+    }
+    return interval;
+  }
+
+  getBlurredPlaneForQuantizedInterval(interval, category) {
+    switch (category) {
+      case "skyscrapers":
+        return blurredSkyscrapers[interval];
+      default:
+        return blurredSkyscrapers[interval];
+    }
   }
 }
 
@@ -154,8 +226,8 @@ class Skyscrapers {
     this.y = 0;
   }
 
-  draw() {
-    image(skyscrapers, this.x, this.y, 500, 500);
+  draw(imageToDraw) {
+    image(imageToDraw, this.x, this.y, 500, 500);
   }
 }
 
@@ -188,6 +260,8 @@ class Man {
       358 * (this.sizeMultiplier % 1)
     );
 
-    text("depth: " + (this.x + this.deltaX), 10, 30);
+    depthOfField = this.x + this.deltaX;
+
+    text("depth: " + depthOfField, 10, 30);
   }
 }
